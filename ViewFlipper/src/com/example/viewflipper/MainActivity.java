66e8455515f8d8577cf02674765e9c6ca40dde58
wsscp <915542393@qq.com>
2016-11-15 16:46:37 +0800

    可以左右滑动

--- ViewFlipper/src/com/example/viewflipper/MainActivity.java
+++ ViewFlipper/src/com/example/viewflipper/MainActivity.java
@@ -5,12 +5,14 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends ActionBarActivity {
 
 	private ViewFlipper flipper;
+	private float startX;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,16 +22,43 @@ public class MainActivity extends ActionBarActivity {
 		flipper.addView(getImageView(R.drawable.pic2));
 		flipper.addView(getImageView(R.drawable.pic3));
 		flipper.addView(getImageView(R.drawable.pic4));	
-		flipper.setInAnimation(this, R.anim.left_in);
-		flipper.setOutAnimation(this, R.anim.left_out);
-		flipper.setFlipInterval(4000);
-		flipper.startFlipping();
+		//自动播放动画效果
+//		flipper.setInAnimation(this, R.anim.left_in);
+//		flipper.setOutAnimation(this, R.anim.left_out);
+//		flipper.setFlipInterval(4000);
+//		flipper.startFlipping();
 	}
 
 	private ImageView getImageView(int resId){
 		ImageView imageView=new ImageView(this);
 		imageView.setBackgroundResource(resId);
 		return imageView;
+	}
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			startX=event.getX();
+			break;
+		case MotionEvent.ACTION_UP:
+			//向右滑动
+			if(event.getX()-startX>100){
+				flipper.setInAnimation(this, R.anim.left_in);
+				flipper.setOutAnimation(this, R.anim.left_out);
+				flipper.showNext();
+			}
+			//向左滑动
+			if(event.getX()-startX<100){
+				flipper.setInAnimation(this, R.anim.right_in);
+				flipper.setOutAnimation(this, R.anim.right_out);
+				flipper.showPrevious();
+			}
+			break;
+		default:
+			break;
+		}
+		return super.onTouchEvent(event);
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
